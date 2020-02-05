ebac97aa799d8c1e45239695f73d91fa04f2319a
Matt Masters <Matt.d.m@hotmail.com>
2020-02-05 14:41:36 -0600

    Update urls.py

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
-from util.generate_world import generate_rooms
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
